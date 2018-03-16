de369651c8b9dac15c30ae35c5c4a9e8529d31c6
David Hunter <davhunter@deloitte.ca>
2018-03-16 13:58:50 -0400

    trying to fix problem

--- src/main/java/com/deloitte/capi/transformers/ExecutionDriverTransformer.java
+++ src/main/java/com/deloitte/capi/transformers/ExecutionDriverTransformer.java
@@ -39,14 +39,14 @@ public class ExecutionDriverTransformer extends AbstractMessageTransformer {
 		String searchTerm = message.getProperty("corporation-name", PropertyScope.INVOCATION);
 
 		// Health Canada
-//		Source hc = new Source("healthcanada", searchTerm);
-//		sources.add(hc);
+		Source hc = new Source("healthcanada", searchTerm);
+		sources.add(hc);
 
 		// Twitter
 		Source t = new Source("twitter", searchTerm);
 		sources.add(t);
 		
-		return sources;
+		return sources.toArray();
 	}
 
 }
